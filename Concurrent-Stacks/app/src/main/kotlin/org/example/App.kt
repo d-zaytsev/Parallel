3d114f22cec445f5f2d5c965e0636b8a00cd090b
dmitriy <st107687@student.spbu.ru>
2024-03-27 09:42:40 +0300

    Rewrite tests

--- Concurrent-Stacks/app/src/main/kotlin/org/example/App.kt
+++ Concurrent-Stacks/app/src/main/kotlin/org/example/App.kt
@@ -8,25 +8,42 @@ import kotlin.math.roundToInt
 fun main() {
 
     // Stacks settings
-    val threadCount = 8
+    val threadCountArray = arrayOf(1, 2, 4, 8, 14, 32)
 
     // Tests settings
-    val time = 2000L
+    val timeArray = arrayOf(1000L, 2000L, 4000L, 8000L)
     val workload = 100L
     val repeats = 5
 
-    val stackArr = arrayOf(TreiberStack<Int>(), EliminationStack(8, 20))
-    println("Stack 1: Treibers Stack (without backoff)\nStack 2: Elimination Backoff Stack\n")
+    println("Treiber`s Stack (without backoff)")
 
-    for (i in stackArr.indices) {
-        println("### Stack ${i + 1}: ")
+    for (time in timeArray) {
+        println("Time: $time")
+        for (threadCount in threadCountArray) {
+            println("Threads: $threadCount")
+            val bench = ProduceConsumeBenchmark(TreiberStack(), workload)
 
-        val bench = ProduceConsumeBenchmark(stackArr[i], workload)
+            val results = Array(repeats) {
+                bench.perform(time, threadCount)
+            }
 
-        val results = Array(repeats) {
-            bench.perform(time, threadCount)
+            println("result: ${results.average().roundToInt()} ops")
         }
+    }
 
-        println("result: ${results.average().roundToInt()} ops")
+    println("Elimination Backoff Stack")
+
+    for (time in timeArray) {
+        println("Time: $time")
+        for (threadCount in threadCountArray) {
+            println("Threads: $threadCount")
+            val bench = ProduceConsumeBenchmark(EliminationStack(threadCount, 100), workload)
+
+            val results = Array(repeats) {
+                bench.perform(time, threadCount)
+            }
+
+            println("result: ${results.average().roundToInt()} ops")
+        }
     }
 }
